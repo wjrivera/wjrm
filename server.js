1fe6ef6b24385fee836004aa4d00ed064367f757
William J Rivera <William J Rivera>
2016-12-18 20:51:17 -0500

    Basic express app deployed

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Working");
+var express = require("express");
+var morgan = require("morgan");
+var app = express("");
+
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+    res.send("William Rivera");
+});
+
+app.listen(8080, function () {
+    console.log("Running on port 8080")
+});
